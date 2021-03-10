e2f4c3c50ffdfa49d8bc32ca3986ad8939262a2b
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-03-11 00:05:11 +0530

    complete the js file to perform POST to api

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -4,12 +4,31 @@ const outputForm = document.querySelector('.output-form');
 const output = document.querySelector('.output');
 const shortenBtn = document.querySelector('.btn');
 
-console.log(output);
+const isValidUrl = (url) => url.protocol === 'http:' || url.protocol === 'https:';
+
 shortenBtn.addEventListener('click', () => {
-    outputForm.classList.add('active');
-    output.value = 'Please, provide a valid url';
-    setTimeout(() => {
-        outputForm.classList.remove('active');
-        output.value = '';
-    }, 2000);
+    const url = input.value;
+    if (!isValidUrl(new URL(url))) {
+        outputForm.classList.add('active');
+        output.value = 'Please, provide a valid url';
+        setTimeout(() => {
+            outputForm.classList.remove('active');
+            output.value = '';
+        }, 2000);
+    } else {
+        fetch('./api/urls', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                longUrl: url,
+            }),
+        })
+            .then((resp) => resp.json())
+            .then((json) => {
+                const shortUrl = `${document.location.origin}/${json.id}`;
+                output.value = shortUrl;
+            });
+    }
 });
