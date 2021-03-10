0899e2cc8f35cad592b722f61502e8646b523030
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-03-10 19:02:13 +0530

    redirected the response

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ const api = require('./api');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const url = require('./url_data');
+const urls = require('./url_data');
 
 app.use(express.json());
 app.use('/api', api);
-app.use('/users', express.static('public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Hello node');
+app.get('/:id', (req, res) => {
+    const response = urls[req.params.id];
+    if (response) {
+        res.redirect(response);
+    } else {
+        res.status(404).send('Invalid URL');
+    }
 });
 
 app.listen(port, () => {
