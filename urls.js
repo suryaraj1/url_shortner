eb7cb8ae36ae7f64bdccc066806feb24a434e5b9
Suryaraj Bhaduri <suryaraj_bhaduri@yahoo.com>
2021-03-10 19:00:13 +0530

    added POST, GET and DELETE methods

--- urls.js
+++ urls.js
@@ -13,4 +13,32 @@ router.get('/', (req, res) => {
     res.status(200).send(urlList);
 });
 
+router.post('/', (req, res) => {
+    const id = shortid.generate();
+    urls[id] = req.body.longUrl;
+    res.status(201).send({ id });
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const longUrl = urls[id];
+    console.log(longUrl);
+    if (longUrl) {
+        return res.status(200).send({ id, longUrl });
+    }
+    return res.status(404).send('Url with given short id not found!');
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    const longUrl = urls[id];
+    console.log(id);
+    if (longUrl) {
+        delete urls[id];
+        res.status(201).send('Deletion successful!');
+    } else {
+        return res.status(404).send('Url with given short id not found!');
+    }
+});
+
 module.exports = router;
